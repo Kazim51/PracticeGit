c66b8d2fc6453d29bc5574669a833008e1bf7adf
Kazim51 <161171484+Kazim51@users.noreply.github.com>
2025-02-24 03:30:57 +0000

    this ismy first git commit

---
+++ main.py
@@ -0,0 +1,4 @@
+coursenName = "CI/CD on AWS"
+candidateName = "Kazim Ali"
+print((f'My name is {candidateName} and I am currently taking the course titled {courseName}')
+This will be an interesting learning curve for me
